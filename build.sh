5ba5af06374c383e2bfb756b8610b469c72c71da
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2020-06-01 16:31:28 -0700

    Update
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 
+export DEBIAN_DATE=2020-06-01
 export TITLE="Librespot"
 export DESCRIPTION="A dubo image for Librespot"
 export IMAGE_NAME="librespot"

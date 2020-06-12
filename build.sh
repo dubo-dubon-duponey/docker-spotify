f6d06e408c36938be1bb65442849b53bb0396613
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2020-06-12 11:30:28 -0700

    Remove armv6
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ export DEBIAN_DATE=2020-06-01
 export TITLE="Librespot"
 export DESCRIPTION="A dubo image for Librespot"
 export IMAGE_NAME="librespot"
+# Building on armv6 takes an excruciating amount of time now (does it even complete?), so, bye bye v6
+export PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
 
 # shellcheck source=/dev/null
 . "$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)/helpers.sh"

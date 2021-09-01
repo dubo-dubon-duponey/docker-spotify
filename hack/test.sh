0af67bc5ef29aaefa6fbf7fbf9d57c1b5fc5fc45
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-09-01 14:51:32 -0700

    Cosmetic
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- hack/test.sh
+++ hack/test.sh
@@ -4,9 +4,6 @@ set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 # shellcheck source=/dev/null
 root="$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)/../"
 
-readonly name="${1:-image}"
-shift || true
-
 # Simple no-thrill build tester
 # XXX Currently reduced to a single architecture to avoid using all disk space until we figure out our space efficiency problem (likely the fat builder image getting duplicated over and over)
 # Solution would probably be to do like buildkit and fetch with a lightweight go image while build mount from the previous stage instead of inheriting - annoying but probably the only way
@@ -16,7 +13,7 @@ if ! "$root/hack/build.sh" \
 	  --inject date=2021-09-01 \
 	  --inject suite=bullseye \
     --inject platforms=linux/arm64 \
-  	"$name" "$@"; then
+  	"image" "$@"; then
   printf >&2 "Failed building\n"
   exit 1
 fi

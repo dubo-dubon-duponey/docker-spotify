ebba6bbccbe3ad0ec5b22ef3b4880844f6a135e4
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-10-18 15:41:32 -0700

    Disable shelling out entirely if no DISPLAY_ENABLED
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- context/runtime/boot/entrypoint.sh
+++ context/runtime/boot/entrypoint.sh
@@ -10,8 +10,12 @@ source "$root/mdns.sh"
 
 helpers::dir::writable /tmp
 
+readonly PORT="${PORT:-10042}"
 # This is purely cached music, so, disposable and transient
-args=(--onevent /boot/onevent.sh --cache-size-limit 8G --cache /tmp/cache --name "${MDNS_NAME:-Sproutify}" --bitrate 320 --device-type speaker --zeroconf-port "${PORT:-10042}")
+args=(--cache-size-limit 8G --cache /tmp/cache --name "${MDNS_NAME:-Sproutify}" --bitrate 320 --device-type speaker --zeroconf-port "$PORT")
+
+# Hook the experimental display script if asked to
+[ ! "$SPOTIFY_CLIENT_ID" ] || [ ! "$SPOTIFY_CLIENT_SECRET" ] || [ ! "$DISPLAY_ENABLED" ] || args+=(--onevent /boot/onevent.sh)
 
 # mDNS blast if asked to
 [ ! "$MDNS_HOST" ] || {
@@ -21,15 +25,10 @@ args=(--onevent /boot/onevent.sh --cache-size-limit 8G --cache /tmp/cache --name
   args+=(--disable-discovery)
 }
 
+
 [ "$(printf "%s" "$LOG_LEVEL" | tr '[:upper:]' '[:lower:]')" != "debug" ] || args+=(--verbose)
 [ ! "$OUTPUT" ] || args+=(--backend "$OUTPUT")
 [ ! "$DEVICE" ] || args+=(--device "$DEVICE")
 args+=("$@")
 
 exec librespot "${args[@]}"
-
-# Nightingale
-# spotify.nightingale.local.
-# 10.0.4.66:10042
-# VERSION=1.0
-# CPath=/
